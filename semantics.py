d1a9f735286d5ccf3a59fa93ad7db62e9d90574e
Emilio <emiliogzz24@gmail.com>
2020-04-25 12:56:27 -0500

    Wrapping up varDirectory

--- semantics.py
+++ semantics.py
@@ -1,10 +1,11 @@
 class Variable:
-  def __init__(self, name, type):
+  def __init__(self, name, type, dimensions):
     self.name = name
     self.type = type
+    self.dimensions = dimensions
 
   def __repr__(self):
-    return "{}, {}".format(self.name, self.type)
+    return "{}, {}, {}".format(self.name, self.type, self.dimensions)
 
 class Function:
   def __init__(self, name, type, vars_table):
@@ -19,24 +20,36 @@ def printDirectory(directory):
   for key in directory:
     print(directory[key])
 
+def getVariable(var):
+  name = var.getChild(2).getText()
+  var_type = var.getChild(0).getText()
+  dimensions = {}
+  if name.count('[') == 1:
+    dimensions['1'] = int(name[name.find('[') + 1:name.find(']')])
+    name = name[:name.find('[')]
+  elif name.count('[') == 2:
+    dimensions['1'] = int(name[name.find('[') + 1:name.find(']')])
+    dimensions['2'] = int(name[name.rfind('[') + 1:name.rfind(']')])
+    name = name[:name.find('[')]
+
+  return name, Variable(name, var_type, dimensions)
+
 def extendVarDirectory(directory, varList):
   for i in range(len(varList)):
-    name = varList[i].getChild(2).getText()
-    var_type = varList[i].getChild(0).getText()
+    name, variable = getVariable(varList[i])
     if name not in directory:
-      directory[name] = Variable(name, var_type)
+      directory[name] = variable
     else:
-      print("intentas declarar la misma variable dos veces!")
+      print("La variable '{}' ya fue declarada anteriormente.".format(name))
 
 def getVarDirectory(varList):
   var_directory = {}
   for i in range(len(varList)):
-    name = varList[i].getChild(2).getText()
-    var_type = varList[i].getChild(0).getText()
+    name, variable = getVariable(varList[i])
     if name not in var_directory:
-      var_directory[name] = Variable(name, var_type)
+      var_directory[name] = variable
     else:
-      print("intentas declarar la misma variable dos veces!")
+      print("La variable '{}' ya fue declarada anteriormente.".format(name))
   return var_directory
 
 function_directory = {}
