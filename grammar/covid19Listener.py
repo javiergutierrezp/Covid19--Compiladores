46d2657bc56d3fd250e255dbe419b0d998b50e03
Emilio <emiliogzz24@gmail.com>
2020-04-26 13:33:22 -0500

    WIP - First quad kind of done, animo

--- grammar/covid19Listener.py
+++ grammar/covid19Listener.py
@@ -85,6 +85,7 @@ class covid19Listener(ParseTreeListener):
     # Enter a parse tree produced by covid19Parser#megaexpresion.
     def enterMegaexpresion(self, ctx:covid19Parser.MegaexpresionContext):
         # print(ctx.getRuleContext().getSourceInterval())
+        
         appendQuads(ctx.getText())
         pass
 
